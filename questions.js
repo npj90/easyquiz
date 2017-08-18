a0f6fe9b39cfd9f98294011d313cf5a97b677171
Nishanth P <npjudson90@gmail.com>
2017-08-18 14:39:07 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,40 @@
+var questions = [{
+  "question": " Who is a 'Superhero'?",
+  "option1": "Brian Zane",
+  "option2": "Tom Payne",
+  "option3": "Bruce Wayne",
+  "option4": "Mike Bane",
+  "answer": "3"
+}, {
+  "question": " Where is the 'Grand Canyon'?",
+  "option1": "Australia",
+  "option2": "Japan",
+  "option3": "Canada",
+  "option4": "USA",
+  "answer": "4"
+},
+{
+  "question": " What is a 'salamander'?",
+  "option1": "Bird",
+  "option2": "Reptile",
+  "option3": "Fish",
+  "option4": "Dinosaur",
+  "answer": "2"
+},
+{
+  "question": " Who wrote 'Oliver Twist'?",
+  "option1": "Charlemange",
+  "option2": "Charles Darwin",
+  "option3": "Charlie Chaplin",
+  "option4": "Charles Dickens",
+  "answer": "4"
+},
+{
+  "question": " Which color is the 'Jolly Roger'?",
+  "option1": "Pink",
+  "option2": "Yellow",
+  "option3": "Black",
+  "option4": "Green",
+  "answer": "3"
+},
+]
